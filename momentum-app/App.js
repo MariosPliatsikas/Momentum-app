23309adf1f4947347bda9d3455d1744cd0635c60
MarPls <35261805+MariosPliatsikas@users.noreply.github.com>
2023-12-28 14:25:13 +0100

    npm_install_axios

--- momentum-app/App.js
+++ momentum-app/App.js
@@ -1,25 +1,16 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View, Button, Alert } from 'react-native';
+import axios from 'axios';
 
 export default function App() {
   const handleRouteOptimization = async () => {
     try {
-      // Simulate a request to the AI service (replace with actual API call)
-      const response = await fetch('https://your-ai-service-endpoint/route-optimization', {
-        method: 'POST',
-        // Include any required headers and request body
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({
-          // Include any relevant data for optimization
-          origin: 'Current Location',
-          destination: "User's Destination",
-          // Additional data as needed
-        }),
+      const response = await axios.post('http://127.0.0.1:5000/route-optimization', {
+        origin: 'Current Location',
+        destination: "User's Destination",
       });
 
-      const result = await response.json();
+      const result = response.data;
 
       // Display the optimized route to the user
       Alert.alert('Optimized Route', `Optimal Route: ${result.route}`);
@@ -29,10 +20,25 @@ export default function App() {
     }
   };
 
+  const handleCreativePrompt = async () => {
+    try {
+      const response = await axios.get('http://127.0.0.1:5000/creative-prompt');
+
+      const prompt = response.data.prompt;
+
+      // Display the creative prompt to the user
+      Alert.alert('Creative Prompt', `Your Creative Prompt: ${prompt}`);
+    } catch (error) {
+      console.error('Error fetching creative prompt:', error.message);
+      Alert.alert('Error', 'Failed to fetch creative prompt. Please try again.');
+    }
+  };
+
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
       <Button title="Optimize Route" onPress={handleRouteOptimization} />
+      <Button title="Get Creative Prompt" onPress={handleCreativePrompt} />
       <StatusBar style="auto" />
     </View>
   );
