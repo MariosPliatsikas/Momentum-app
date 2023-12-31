cd092f6319b9942128c2cebd868d58b632d6f914
MarPls <35261805+MariosPliatsikas@users.noreply.github.com>
2023-12-31 13:50:44 +0100

    03

--- momentum-app/services/WeatherService.js
+++ momentum-app/services/WeatherService.js
@@ -1,7 +1,7 @@
 // WeatherService.js
 import axios from 'axios';
 
-const API_KEY = '0db4de094b4a1d677bd34f5e7dbdf7c0'; // Replace with your actual API key
+const API_KEY = 'API_key'; // Replace with your actual API key
 const BASE_URL = 'https://api.openweathermap.org/data/2.5/forecast';
 
 export const getWeather = async (lat, lon) => {
